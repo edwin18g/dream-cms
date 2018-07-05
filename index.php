1dcf62942b1789665e1b5b53637dc941ebc1922b
edwin18g <edwin18g@gmail.com>
2018-07-05 23:44:38 +0530

    New
    Tes

--- index.php
+++ index.php
@@ -55,11 +55,7 @@ window.onpopstate = function(e) {
 };
 
   </script>
-<div class="container">
-  <h1>My First Bootstrap Page</h1>
-  <p>This part is inside a .container class.</p> 
-  <p>The .container class provides a responsive fixed width container.</p>           
-</div>
+
 
 </body>
 </html>
