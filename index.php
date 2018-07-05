df5a498f5049fde2be48eb38263f90485f1ce05d
edwin18g <edwin18g@gmail.com>
2018-07-05 12:56:20 +0530

    js
    js route

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
-<?php
-echo "hia";
-/*
-session_start(); 
-//new git
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  
+<div class="container">
+  <h1>My First Bootstrap Page</h1>
+  <p>This part is inside a .container class.</p> 
+  <p>The .container class provides a responsive fixed width container.</p>           
+</div>
 
-// Defines
-define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
-define('APP_DIR', ROOT_DIR .'application/');
-require(ROOT_DIR .'system/press.php');
-
-// Define base URL
-global $config;
-define('BASE_URL', $config['base_url']);
-press();*/
-
-?>
+</body>
+</html>
