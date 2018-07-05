3a11aff4b33c9d2d9736bc1296ba221f9f9f6670
edwin18g <edwin18g@gmail.com>
2018-07-05 16:48:24 +0530

    05*06-2018
    tese

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
   <script>
   // this should be the Ajax Method.
 // and load the url content
-    document.getElementById("myFrame").onload = function() {setCurrentPage(window.location.pathname)};
+    document.onload = function() {setCurrentPage(window.location.pathname)};
 var setCurrentPage = function(url) {
     $('app-root').html(url);
   $('title').html(url);
