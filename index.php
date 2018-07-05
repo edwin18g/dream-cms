28cfbb5578e45b9ccfeaa3332221dfa2e398b8f9
edwin18g <edwin18g@gmail.com>
2018-07-05 14:02:35 +0530

    nav add
    nav bar added

--- index.php
+++ index.php
@@ -9,6 +9,19 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+  <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
   <script>
   // this should be the Ajax Method.
 // and load the url content
